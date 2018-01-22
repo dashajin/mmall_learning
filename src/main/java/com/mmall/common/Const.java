c24f4c1204470d3c5a4879940e2b9ba053ee8d14
dashajin <dashajin@126.com>
2018-01-23 01:01:47 +0800

    complete alipay

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -23,14 +23,14 @@ public class Const {
         Set<String> PRICE_ASC_DESC = Sets.newHashSet("price_desc", "price_asc");
     }
 
-    public interface Cart{
+    public interface Cart {
         int CHECKED = 1;
         int UNCHECKED = 0;
 
         String LIMIT_NUM_FAIL = "LIMIT_NUM_FAIL";
         String LIMIT_NUM_SUCCESS = "LIMIT_NUM_SUCCESS";
     }
-    public enum ProductStatusEnum{
+    public enum ProductStatusEnum {
         ON_SALE(1, "在线");
         private String value;
         private int code;
@@ -38,6 +38,55 @@ public class Const {
             this.code = code;
             this.value = value;
         }
+
+        public String getValue() {
+            return value;
+        }
+
+        public int getCode() {
+            return code;
+        }
+    }
+
+    public enum OrderStatusEnum {
+        CANCELED(0, "已取消"),
+        NO_PAY(10, "未支付"),
+        PAID(20, "已付款"),
+        SHIPPED(40, "已发货"),
+        ORDER_SUCCESS(50, "订单完成"),
+        ORDER_CLOSE(60, "订单关闭");
+        OrderStatusEnum(int code, String value) {
+            this.code = code;
+            this.value = value;
+        }
+        private String value;
+        private int code;
+
+        public String getValue() {
+            return value;
+        }
+
+        public int getCode() {
+            return code;
+        }
+    }
+
+    public interface AlipayCallback {
+        String TRADE_STATUS_WAIT_BUYER_PAY = "WAIT_BUYER_PAY";
+        String TRADE_STATUS_TRADE_SUCCESS = "TRADE_SUCCESS";
+        String RESPONSE_SUCCESS = "success";
+        String RESPONSE_FAILED = "failed";
+
+    }
+
+    public enum PayPlatformEnum {
+        ALIPAY(1, "支付宝");
+        PayPlatformEnum (int code, String value) {
+            this.code = code;
+            this.value = value;
+        }
+        private String value;
+        private int code;
 
         public String getValue() {
             return value;
